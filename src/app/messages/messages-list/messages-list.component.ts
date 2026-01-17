e2c71ef66398d37192fbe1824b72ec137e148e4c
Lan Nguyen <str4wbxrryymilk@gmail.com>
2026-01-17 15:19:47 +0700

    end of section 10

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,3 @@
-import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 import { MessagesService } from '../messages.service';
@@ -6,14 +5,13 @@ import { MessagesService } from '../messages.service';
 @Component({
   selector: 'app-messages-list',
   standalone: true,
-  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-  messages$ = this.messagesService.messages$;
+  messages = this.messagesService.allMessages;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
